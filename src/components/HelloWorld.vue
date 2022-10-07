c82ae5aaad3fceab39b7f1668414f8e93e6c1186
ubuntu <yuzhenpin@gmail.com>
2022-10-07 05:45:16 +0000

    prevent to reload whole page

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,31 @@
 <template>
   <v-container>
+    <v-row>
+      <v-col
+        cols="12"
+        md="4"
+      >
+        <v-form ref="form" @submit.prevent="submit">
+          <v-text-field
+            v-model="model"
+            ref="fieldId"
+            @keydown.enter="onSubmit"
+            :rules="rules"
+            :label="chinese"
+          ></v-text-field>
+        </v-form>
+      </v-col>
+
+      <v-col
+        cols="4"
+        md="4"
+      >
+      <v-btn tile color="success" @click="submit">
+        <v-icon left> mdi-pencil </v-icon>
+        下一个单词
+        </v-btn>
+      </v-col>
+    </v-row>
     <v-row
       justify="space-between"
     >
@@ -7,62 +33,40 @@
         cols="12"
         md="4"
       >
-        <v-form ref="form">
-          <v-text-field
-            v-model="model"
-            ref="fieldId"
-            :counter="max"
-            :rules="rules"
-            :label="label"
-          ></v-text-field>
-        </v-form>
+      <v-alert v-if="done" type="success">正确： {{done.data.length}} 个英文单词</v-alert>
+      <v-alert v-if="fail" type="error">错误： {{fail.data.length}} 个英文单词</v-alert>
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
+  import axios from 'axios';
   export default {
-    props: {
-      label: {
-        type: String,
-      },
-      value: {
-        type: String,
-      }
-    },
     data: () => ({
       allowSpaces: false,
-      max: 0,
       model: '',
+      id: null,
+      english: '',
+      chinese: '',
+      done: null,
+      fail: null
     }),
     mounted() {
+      this.loadData();
       this.$refs.fieldId.focus();
     },
     computed: {
       rules () {
         const rules = []
 
-        if (this.max) {
-          const rule =
-            v => (v || '').length <= this.max ||
-              `A maximum of ${this.max} characters is allowed`
-
-          rules.push(rule)
-        }
-
-        if (!this.allowSpaces) {
-          const rule =
-            v => (v || '').indexOf(' ') < 0 ||
-              'No spaces are allowed'
-
-          rules.push(rule)
-        }
-
-        if (this.value) {
-          const rule =
-            v => (v || '').toLowerCase() === this.value.toLowerCase() ||
-              'Oops, 错了哟 :('
+        if (this.english) {
+          const rule = v => {
+            if (v && v.toLowerCase() !== this.english.toLowerCase()) {
+              return 'Oops, 错了哟 :(';
+            }
+            return true;
+          }
 
           rules.push(rule)
         }
@@ -70,17 +74,43 @@
         return rules
       },
     },
-
     watch: {
-      value: 'validateField',
-      max: 'validateField',
       model: 'validateField',
     },
-
     methods: {
       validateField () {
         this.$refs.form.validate()
       },
+      async loadData() {
+        this.model = '';
+        const resp = await axios.get('/api/words/todo');
+        const words = resp.data;
+        const index = this.getRandomInt(words.length);
+        const word = words[index];
+        this.id = word.id;
+        this.english = word.english;
+        this.chinese = word.chinese;
+        this.fail = await axios.get('/api/words/fail');
+        this.done = await axios.get('/api/words/done');
+        window.console.log(JSON.stringify(word));
+      },
+      onSubmit() {
+        if (this.model.toLowerCase() === this.english.toLowerCase()) {
+          axios.put(`/api/words/${this.id}/right`, {}).then(res => {
+            window.console.log(res);
+          });
+        } else {
+          axios.put(`/api/words/${this.id}/wrong`, {}).then(res => {
+            window.console.log(res);
+          });
+        }
+      },
+      submit() {
+        this.loadData();
+      },
+      getRandomInt(max) {
+        return Math.floor(Math.random() * max);
+      }
     },
   }
 </script>
--- src/components/StudyWords.vue
+++ src/components/StudyWords.vue
@@ -1,47 +1,13 @@
 <template>
   <v-container>
-    <HelloWorld v-for="word in words" :key="word.key" :label="word.chinese" :value="word.english" />
+    <HelloWorld/>
   </v-container>
 </template>
 
 <script>
 import HelloWorld from './HelloWorld.vue';
-import WORDS from '../words.json';
 
 export default {
   components: { HelloWorld },
-  data() {
-    return {
-      words: this.getIndexes(1, WORDS.length).map((i, key) => {
-        return {
-          key,
-          input: "",
-          message: "",
-          ...WORDS[i]
-        }
-      }),
-    } 
-  },
-  methods: {
-    getIndexes(size, max) {
-      const indexes = [];
-      if (size >= max) {
-        for (let i = 0; i < size; i++) {
-            indexes.push(i);
-        }
-        return indexes;
-      }
-      while (indexes.length < size) {
-        const i = this.getRandomInt(max);
-        if (!indexes.includes(i)) {
-            indexes.push(i);
-        }
-      }
-      return indexes;
-    },
-    getRandomInt(max) {
-      return Math.floor(Math.random() * max);
-    }
-  },
 }
 </script>
